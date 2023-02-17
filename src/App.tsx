5c7eaa66c05724c09f6c71fd29122b508da2d45d
bralash <emmanuelasaber@gmail.com>
2023-02-17 01:21:44 +0000

    Completed folder structure setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './App.css';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 
 function App() {
   return (
