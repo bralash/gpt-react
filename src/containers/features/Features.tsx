da4dd318d577983a70a797c193f0b5285a66ff29
bralash <emmanuelasaber@gmail.com>
2023-02-17 01:16:06 +0000

    Added all container files

---
+++ src/containers/features/Features.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './features.css'
+
+const Features = () => {
+    return (
+        <>
+            <h1>Features</h1>
+        </>
+    );
+}
+
+export default Features
